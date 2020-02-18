e910f4e7da4b96d8a4a631ac09ef84fede1679ec
Devki1 <gdev3195@gmail.com>
2020-02-18 18:13:27 +0530

    check leap year or not

---
+++ ifelse_statement/leap.sh
@@ -0,0 +1,14 @@
+echo "enter a year"
+read year
+if (($year >= 1000  && $year <=9999))
+then 
+	echo "valid $year"
+	if (( $year % 4 ==0  && $year % 100!= 0 || $year % 400 == 0 ))
+	then
+ 		echo $year leap year
+	else
+ 		echo $year not leap year
+	fi
+else
+echo "not valid"
+fi
