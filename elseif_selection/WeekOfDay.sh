0f39e0c1b1fd9378d1832a09110efd447bc1f9ec
Devki1 <gdev3195@gmail.com>
2020-02-19 13:22:08 +0530

    display week of the day

---
+++ elseif_selection/WeekOfDay.sh
@@ -0,0 +1,28 @@
+read -p "enter a number:" num
+if (( $num < 7 ))
+  then
+    if (( $num == 0 ))
+    then
+       echo "sunday"
+     elif (( $num == 1 ))
+     then 
+       echo "monday"
+     elif (( $num == 2 ))
+     then 
+       echo "tuesday"
+     elif (( $num == 3 ))
+     then 
+       echo "wednesday"
+     elif (( $num == 4 ))
+     then 
+       echo "thursday"
+     elif (( $num == 5 ))
+     then 
+       echo "friday"
+     elif (( $num == 6 ))
+     then 
+       echo "saturday"
+    else
+       echo "invalid" 
+    fi
+fi   
