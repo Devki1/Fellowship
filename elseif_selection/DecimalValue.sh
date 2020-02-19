fa49005459574f2ebd3c3c3c3136854bfb6e4bc7
Devki1 <gdev3195@gmail.com>
2020-02-19 13:11:27 +0530

    display place value

---
+++ elseif_selection/DecimalValue.sh
@@ -0,0 +1,77 @@
+read -p "enter a number:" num
+if ((${#num} ==4))
+then
+   if ((${#num} == 4))
+   then 
+      echo "unit"
+      temp=$((num%10))
+      echo "$temp"
+      num=$((num/10))
+   fi
+   if ((${#num} == 3))
+   then
+     echo "Ten"
+     temp=$((num%10))
+     echo "$temp"
+     num=$((num/10))
+   fi
+   if ((${#num} == 2))
+   then
+       echo "hundred"
+       temp=$((num%10))
+       echo "$temp"
+       num=$((num/10))
+   fi
+   if ((${#num} == 1))
+   then
+        echo "thousand"
+        temp=$((num%10))
+        echo "$temp"
+        num=$((num/10))
+   fi
+elif ((${#num} == 3))
+then
+  if ((${#num} == 3)) 
+  then
+      echo "unit"
+      temp=$((num%10))
+      echo "$temp"
+      num=$((num/10))
+fi
+    if ((${#num} == 2))
+    then
+       echo "tens"
+      temp=$((num%10))
+      echo"$temp"
+      num=$((num/10))
+fi
+    if ((${#num} == 1))
+    then
+       echo "hundred"
+       temp=$((num%10))
+       echo "$temp"
+       num=$((num/10))
+fi
+elif ((${#num} == 2))
+then
+   if ((${#num} == 2))
+   then
+       echo "unit"
+      temp=$((num%10))
+      echo "$temp"
+      num=$((num/10))
+fi
+   if ((${#num} == 1))
+    then
+       echo "ten"
+       temp=$((num%10))
+       echo"$temp"
+      num=$((num/10))
+fi
+elif ((num == 1))
+then
+   echo "unit"
+else
+echo "invalid"
+fi
+      
