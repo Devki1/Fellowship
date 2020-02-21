7f7ffcdc72e874b440be4833ca60f0008ab3e084
Devki1 <gdev3195@gmail.com>
2020-02-21 11:03:29 +0530

    Added:flipcoin.sh

---
+++ whileloop_repetition/FlipCoinUsingWhileLoop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+tail=0
+head=0
+while [[ $head -lt 11 && $tail -lt 11 ]]
+do
+	random=$(( RANDOM%2 ))
+	if(( $random == 1 ))
+	then
+		echo "head"
+		((head++))
+		echo "$head"
+	else
+		echo "tail"
+		((tail++))
+		echo  "$tail"
+         fi
+done
+	if(($head == 11))
+	then
+		echo "$headwin"
+	else
+		echo "tailwin"
+fi
