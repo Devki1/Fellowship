3efc350e312a707c3996dc24d08ae226463e8f2a
Devki1 <gdev3195@gmail.com>
2020-02-21 11:06:25 +0530

    print the table of power two

---
+++ whileloop_repetition/TableOfPowerTwo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+echo "enter a number"
+read number
+power=1
+count=1
+while [[ $power -lt 256 && $count -lt $((number+1)) ]]
+do
+	power=$((2*power))
+	echo "$power"
+	((count++))
+done
