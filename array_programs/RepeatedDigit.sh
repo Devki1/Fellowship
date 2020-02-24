352e194df1dad0c4e0accb0c812c2188f7eb587f
Devki1 <gdev3195@gmail.com>
2020-02-24 18:20:03 +0530

    MODIFIED: Added comments and indentation

--- array_programs/RepeatedDigit.sh
+++ array_programs/RepeatedDigit.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
+#Find the digit that are repeated twice in between 1 to 99
+#declare variable 
 count=0
-for((i=1;i<100;i++))
+#Iterate the value from 1 to 100
+for((iteration=1;iteration<100;iterartion++))
 do
-	temp=$i
-	  tensdigit=$(($temp%10))
-		temp=$(($temp/10))
-	 	unitdigit=$temp
-	if(($tensdigit == $unitdigit))
+	temp=$iteration
+	tens=$(($temp%10))
+	temp=$(($temp/10))
+	unit=$temp
+	#compare tensdigit and unitdigit
+	if(($tens == $unit))
 	then
-		arraynumber[$count]=$i
+		ArrayNumber[$count]=$iteration
 			((count++))
 	fi
 done
-	echo ${arraynumber[@]}
+	echo ${ArrayNumber[@]}
