daa8575f917a1061892e84d8874fb95a1db4ea6a
Devki1 <gdev3195@gmail.com>
2020-02-24 19:41:43 +0530

    MODIFIED:Added comments and indentation

--- elseif_selection/ArithmaticOperationMaxMin.sh
+++ elseif_selection/ArithmaticOperationMaxMin.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
+#To perform arithmatic operation according to question
 read -p "enter a number a: " a
 read -p "enter a number b: " b
 read -p "enter a number c: " c
 num1=`echo "scale=2 ; $a + $b * $c " | bc -l`
-min=$num1
+min=$number1
 max=$num1
 num2=`echo "scale=2 ; $c + $a / $b" | bc -l`
 num3=`echo "scale=2 ; $a % $b + $c" | bc -l`
@@ -12,21 +13,21 @@ echo "num1:"=$num1
 echo "num2:"=$num2
 echo "num3:"=$num3
 echo "num4:"=$num4
-#to find max
+#to find max value
 if((`echo "$max <= $num2" | bc -q`==1))
 then 
-     max=$num2
+	max=$num2
 elif((`echo "$max <= $num3" | bc -q`==1))
 then 
-    max=$num3
+	max=$num3
 elif((`echo "$max <= $num4" | bc -q`==1))
 then
-   max=$num4
+	max=$num4
 fi
-#to find minimum
+#to find minimum value
 if((`echo "$min >= $num2" | bc -q`==1))
 then 
-     min=$num2
+	min=$num2
 fi
 if((`echo "$min >= $num3" | bc -q`==1))
 then 
