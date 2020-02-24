05750ff65b7eb641c9bd4204b19d9f2066406f11
Devki1 <gdev3195@gmail.com>
2020-02-24 19:51:13 +0530

    MODIFIED:Added comments and indentation

--- elseif_selection/WeekOfDay.sh
+++ elseif_selection/WeekOfDay.sh
@@ -1,29 +1,30 @@
 #!//bin/bash -x
-read -p "enter a number:" num
-if (( $num < 7 ))
-  then
-    if (( $num == 0 ))
-    then
-       echo "sunday"
-     elif (( $num == 1 ))
-     then 
-       echo "monday"
-     elif (( $num == 2 ))
-     then 
-       echo "tuesday"
-     elif (( $num == 3 ))
-     then 
-       echo "wednesday"
-     elif (( $num == 4 ))
-     then 
-       echo "thursday"
-     elif (( $num == 5 ))
-     then 
-       echo "friday"
-     elif (( $num == 6 ))
-     then 
-       echo "saturday"
-    else
-       echo "invalid" 
-    fi
+#Take a input from a user
+read -p "enter a Days:" Day
+if (( $Day < 7 ))
+then
+if (( $Day == 0 ))
+then
+	echo "Sunday"
+elif (( $Day == 1 ))
+then 
+	echo "Monday"
+elif (( $Day == 2 ))
+then 
+	echo "Tuesday"
+elif (( $Day == 3 ))
+then 
+	echo "Wednesday"
+elif (( $Day == 4 ))
+then 
+	echo "Thursday"
+elif (( $Day == 5 ))
+then 
+	echo "Friday"
+elif (( $Day == 6 ))
+then 
+	echo "Saturday"
+else
+	echo "Invalid" 
+fi
 fi   
