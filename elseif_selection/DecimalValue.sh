ac19294ba47145ae94ad87764b044a3c160dfe74
Devki1 <gdev3195@gmail.com>
2020-02-24 19:47:21 +0530

    MODIFIED:Added comments and indentation

--- elseif_selection/DecimalValue.sh
+++ elseif_selection/DecimalValue.sh
@@ -1,78 +1,79 @@
 #!/bin/bash -x
+#To print the value with respect to digit place
 read -p "enter a number:" num
-if ((${#num} ==4))
+if ((${#num} == 4))
 then
-   if ((${#num} == 4))
-   then 
-      echo "unit"
-      temp=$((num%10))
-      echo "$temp"
-      num=$((num/10))
-   fi
-   if ((${#num} == 3))
-   then
-     echo "Ten"
-     temp=$((num%10))
-     echo "$temp"
-     num=$((num/10))
-   fi
-   if ((${#num} == 2))
-   then
-       echo "hundred"
-       temp=$((num%10))
-       echo "$temp"
-       num=$((num/10))
-   fi
-   if ((${#num} == 1))
-   then
-        echo "thousand"
-        temp=$((num%10))
-        echo "$temp"
-        num=$((num/10))
-   fi
+	if ((${#num} == 4))
+	then 
+		echo "unit"
+		temp=$((num%10))
+		echo "$temp"
+		num=$((num/10))
+	fi
+	if ((${#num} == 3))
+	then
+		echo "Ten"
+		temp=$((num%10))
+		echo "$temp"
+		num=$((num/10))
+	fi
+	if ((${#num} == 2))
+	then
+		echo "hundred"
+		temp=$((num%10))
+		echo "$temp"
+		num=$((num/10))
+	fi
+	if ((${#num} == 1))
+	then
+		echo "thousand"
+		temp=$((num%10))
+		echo "$temp"
+		num=$((num/10))
+	fi
 elif ((${#num} == 3))
 then
-  if ((${#num} == 3)) 
-  then
-      echo "unit"
-      temp=$((num%10))
-      echo "$temp"
-      num=$((num/10))
-fi
-    if ((${#num} == 2))
-    then
-       echo "tens"
-      temp=$((num%10))
-      echo"$temp"
-      num=$((num/10))
-fi
-    if ((${#num} == 1))
-    then
-       echo "hundred"
-       temp=$((num%10))
-       echo "$temp"
-       num=$((num/10))
-fi
+	if ((${#num} == 3)) 
+	then
+		echo "unit"
+		temp=$((num%10))
+		echo "$temp"
+		num=$((num/10))
+	fi
+	if ((${#num} == 2))
+	then
+		echo "tens"
+		temp=$((num%10))
+		echo"$temp"
+		num=$((num/10))
+	fi
+	if ((${#num} == 1))
+	then
+		echo "hundred"
+		temp=$((num%10))
+		echo "$temp"
+		num=$((num/10))
+	fi
 elif ((${#num} == 2))
-then
-   if ((${#num} == 2))
-   then
-       echo "unit"
-      temp=$((num%10))
-      echo "$temp"
-      num=$((num/10))
-fi
-   if ((${#num} == 1))
-    then
-       echo "ten"
-       temp=$((num%10))
-       echo"$temp"
-      num=$((num/10))
-fi
+	then
+		if ((${#num} == 2))
+		then
+			echo "unit"
+			temp=$((num%10))
+			echo "$temp"
+			num=$((num/10))
+		fi
+		if ((${#num} == 1))
+		then
+			echo "ten"
+			temp=$((num%10))
+			echo"$temp"
+			num=$((num/10))
+		fi
 elif ((num == 1))
 then
-   echo "unit"
+	echo "unit"
 else
-echo "invalid"
+	echo "invalid"
 fi
       
