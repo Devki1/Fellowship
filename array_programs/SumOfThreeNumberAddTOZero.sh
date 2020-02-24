1863b7b3173037baaf83d5b7b066791facd0152a
Devki1 <gdev3195@gmail.com>
2020-02-24 18:24:23 +0530

    MODIFIED: Added comments and indentation

--- array_programs/SumOfThreeNumberAddTOZero.sh
+++ array_programs/SumOfThreeNumberAddTOZero.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -x
+#Find the sum of three digit number  is zero
 read -p "enter the size of array:" length
 echo "size of $length"
-for(( i=0;i<length;i++))
+#Reading element array from user
+for(( iter=0;iter<length;iter++))
 do
-		read array_read
-		array[$i]=$array_read
-done
-for (( i=0; i<$length-2; i++)) 
-    do
-        for ((j=$i+1; j<$length-1; j++)) 
-        do
-            for ((k=$j+1; k<$length; k++)) 
-            do
-                if ((`echo ${array[i]}+${array[j]}+${array[k]} == 0 | bc -q`==1))
-			then
-				echo  "${array[i]} ${array[j]} ${array[k]}" 
-		fi
-	  done
+	read array_read
+	array[$iter]=$array_read
+done	#checking possible triplet
+	for (( iter=0; iter<$length-2; iter++)) 
+	do
+		for ((j=$i+1; j<$length-1; j++)) 
+		do
+			for ((k=$j+1; k<$length; k++)) 
+			do
+				if ((`echo ${array[iter]}+${array[j]}+${array[k]} == 0 | bc -q`==1))
+				then
+					echo "${array[iter]} ${array[j]} ${array[k]}" 
+				fi
+			done
 	done
 done
