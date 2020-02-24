121882c2b8ac3f00f57f02b168324f63a97d6b61
Devki1 <gdev3195@gmail.com>
2020-02-24 19:54:10 +0530

    MODIFIED:Added comments and indentation

--- functionsproblem/PalindromeByUsingFunction.sh
+++ functionsproblem/PalindromeByUsingFunction.sh
@@ -1,23 +1,25 @@
 #!/bin/bash -x
+#Check two number are palindrome or not
 read -p 'enter a value:' value1
 read -p 'enter a value:' value2
 function ispalindrome()
-	{   
-		number1=$1
-		number2=$2
-		reverse=0
-		while (($number1>0))
-		do
-			temp=$((number1 % 10))
-			reverse=$((reverse*10+temp))
-			number1=$((number1 / 10))	
-		done
-		if (( $reverse == $number2 ))
-		then
-				echo "palindrome"
-		else
-				echo "not palindrome"
-	 	fi
-	}
+{   
+	number1=$1
+	number2=$2
+	reverse=0
+	while (($number1>0))
+	do
+		temp=$((number1 % 10))
+		reverse=$((reverse*10+temp))
+		number1=$((number1 / 10))	
+	done
+	if (( $reverse == $number2 ))
+	then
+		echo "palindrome"
+	else
+		echo "not palindrome"
+	fi
+}
+#Store the all the value in result1 and call the ispalindrome function
 result1="$( ispalindrome $value1 $value2 )"
 echo "$result1"
