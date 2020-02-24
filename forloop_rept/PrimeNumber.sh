7a6828b4169bd68d4c4814f4a14d9d39af1c521d
Devki1 <gdev3195@gmail.com>
2020-02-24 19:36:58 +0530

    MODIFIED:Added comments and indentation

--- forloop_rept/PrimeNumber.sh
+++ forloop_rept/PrimeNumber.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -x
+#Print the prime number
 echo "enter a number"
-read number
+read number 
+#Let consider flag=0 
 flag=0
-		if(($number == 1))
+if(($number == 1))
+then
+	flag=1
+fi
+#Logic for prime number
+for(( iter=2; iter<=$number/2; iter++))
+	do
+		if(( number%iter == 0 ))
 		then
 			flag=1
 		fi
-		for(( num=2; num<=$number/2; num++))
-		do
-			if(( number%num == 0 ))
-			then
-				flag=1
-		fi
-		done
-		if(($flag==0))
-    		then
-			echo "primenumber"
-		else
-			echo "not primenumber"
-		fi
+	done
+if(($flag==0))
+then
+	echo "prime number"
+else
+	echo "not prime number"
+fi
