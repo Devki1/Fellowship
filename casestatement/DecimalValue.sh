3d219fd7be81fe727c18a6e0c69b667b2e2c7f93
Devki1 <gdev3195@gmail.com>
2020-02-25 10:15:27 +0530

    MODIFIED:Added comment and indentation

--- casestatement/DecimalValue.sh
+++ casestatement/DecimalValue.sh
@@ -1,80 +1,81 @@
 #!/bin/bash -x
-read -p "enter a number:" number
+#find the value of digit place
+read -p "enter a numberless/equal 9999:" number
 case ${#number} in
 	4)
-		case ${#number} in
+	case ${#number} in
 		4)
-			echo "unit"
-			temp=$((number%10))
-			echo "$temp"
-			number=$((number/10))
-						;;
-		esac
-		case ${#number} in
+		echo "unit"
+		temp=$((number%10))
+		echo "$temp"
+		number=$((number/10))
+			;;
+	esac
+	case ${#number} in
 		3)
-			temp=$((number%10))
-			echo "$temp-ten"
-			number=$((number/10))
-						;;
-		esac
-	 	case ${#number} in
-     		 2)
-	      		temp=$((number%10))
-      			echo "$temp-hundred"
-      			number=$((number/10))
-						;;
-		esac
-		case ${#number} in
-      		1)
-      			temp=$((number%10))
-      			echo "$temp-thousand"
-      			number=$((number/10))
-		;;
-		esac
+		temp=$((number%10))
+		echo "$temp-ten"
+		number=$((number/10))
+			;;
+	esac
+	case ${#number} in
+		2)
+		temp=$((number%10))
+		echo "$temp-hundred"
+		number=$((number/10))
+			;;
+	esac
+	case ${#number} in
+		1)
+		temp=$((number%10))
+		echo "$temp-thousand"
+		number=$((number/10))
+			;;
+	esac
 	;;
 	3)
-		case ${#number} in
+	case ${#number} in
 		3)
-		 	 temp=$((number%10))
-     			echo "$temp-unit"
-     			 number=$((number/10))
-						;;
-		esac
-		case ${#number} in
-     		2)
-		      temp=$((number%10))
-		      echo "$temp-ten"
-      			number=$((number/10))
-						;;
-		esac
-		case ${#number} in
+		temp=$((number%10))
+		echo "$temp-unit"
+		number=$((number/10))
+			;;
+	esac
+	case ${#number} in
+		2)
+		temp=$((number%10))
+		echo "$temp-ten"
+		number=$((number/10))
+			;;	
+	esac
+	case ${#number} in
 		1)
-     		 temp=$((number%10))
-     		 echo "$temp-ten"
-      		number=$((number/10))
-		;;
-		esac
+		temp=$((number%10))
+		echo "$temp-ten"
+		number=$((number/10))
+			;;
+	esac
 	;;
 	2)
-   		case ${#number} in
-      		2)
-      		temp=$((number%10))
-      		echo "$temp-unit"
-      		number=$((number/10))
-					;;
- 		esac
-		case ${#number} in
+	case ${#number} in
+		2)
+		temp=$((number%10))
+		echo "$temp-unit"
+		number=$((number/10))
+			;;
+	esac
+	case ${#number} in
 		1)
-      		temp=$((number%10))
-      		echo "$temp-ten"
-      		number=$((number/10))
-		;;
-		esac
+		temp=$((number%10))
+		echo "$temp-ten"
+		number=$((number/10))
+			;;
+	esac
 	;;
 	1)
-		echo "$numberunit"
-	;;
+	echo "$numberunit"
+			;;
 	*)
-		echo"out of range"
-	;;
+	echo"out of range"
+			;;
 esac
