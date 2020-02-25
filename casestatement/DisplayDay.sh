9d5f5085e636a88986395a898e2cff0b31cd6593
Devki1 <gdev3195@gmail.com>
2020-02-25 10:21:35 +0530

    MODIFIED:Added comment and indentation

--- casestatement/DisplayDay.sh
+++ casestatement/DisplayDay.sh
@@ -1,29 +1,30 @@
 #!/bin/bash -x
+#Display the week of the day
 echo "enter a number"
 read number
 case $number in
 		0)
 			echo	"Sunday"
-						;;
+			;;
 		1)
 			echo	"Monday"
-						;;
+			;;
 		2)
 			echo	"Tuesday"
-						;;
+			;;
 		3)
 			echo	"Wednesday"
-						;;
+			;;
 		4)
 			echo	"Thursday"
-						;;
+			;;
 		5)
 			echo "Friday"
-						;;
+			;;
 		6)
 			echo "Saturday"
-						;;
+			;;
 		*)
-			echo "invalid"
-		esac
+			echo "Invalid"
+esac
 		
