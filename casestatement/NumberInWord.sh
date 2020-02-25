10c2bf0b845d707619f109577772c9d28f1a201b
Devki1 <gdev3195@gmail.com>
2020-02-25 10:22:58 +0530

    MODIFIED:Added comments and indentation

--- casestatement/NumberInWord.sh
+++ casestatement/NumberInWord.sh
@@ -1,39 +1,39 @@
 #!/bin/bash -x
+#Convert a single digit into a word
 echo "enter a single digit number"
 read number
 case $number in
-         0)
-         echo "Zero"
-               ;;
-        1)
-         echo "one"
-               ;;
-        2)
-         echo "two"
-               ;;
-        
-        3)
-         echo "three"
-               ;;
-        4)
-         echo "four"
-               ;;
-        5)
-         echo "five"
-               ;;
-        6)
-         echo "six"
-               ;;
-        7)
-         echo "seven"
-               ;;
-        8)
-         echo "seven"
-               ;;
-        9)
-         echo "seven"
-               ;;
-        *)
-         echo "invalid"
-      esac
+	0)
+		echo "Zero"
+		;;
+	1)
+		echo "one"
+		;;
+	2)
+		echo "two"
+		;;
+	3)
+		echo "three"
+		;;
+	4)
+		echo "four"
+		;;
+	5)
+		echo "five"
+		;;
+	6)
+		echo "six"
+		;;
+	7)
+		echo "seven"
+		;;
+	8)
+		echo "seven"
+		;;
+	9)
+		echo "seven"
+		;;
+	*)
+		echo "invalid"
+esac
   
