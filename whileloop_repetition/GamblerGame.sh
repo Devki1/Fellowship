443ad8135b8be9292bd7218cf1091622b400df2d
Devki1 <gdev3195@gmail.com>
2020-02-28 09:17:10 +0530

    MODIFIED:Added comments and indentation

--- whileloop_repetition/GamblerGame.sh
+++ whileloop_repetition/GamblerGame.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -x
-echo "gambler game"
-money=100
+echo "Gambler game"
+stake=100
 goal=200
 win=0
 loss=0
-while (( money<=goal && money>0 ))
+while (( $stake<=$goal && $stake>0 ))
 do
-		randomnumber=$((RANDOM%2))
-		if(($randomnumber == 1))
-		then
-				echo "money  is win"
-				money=$((money+1))
-					((win++))
-		else
-				echo "loss money"
-				money=$((money-1))
-					((loss++))
-		fi
+	randomnumber=$((RANDOM%2))
+	if(($randomnumber == 1))
+	then
+		echo "You stake  is win"
+		stake=$((stake+1))
+			((win++))
+	else
+		echo "You loss stake"
+		stake=$(($stake-1))
+			((loss++))
+	fi
 done
-		if((money == 0))
-		then
-			echo "you broke"
-		fi
-	echo "win: $win"
-	echo "loss:$loss"
+if(($stake == 0))
+then
+	echo "You are broke"
+fi
+echo "Win: $win"
+echo "Loss:$loss"
