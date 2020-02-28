449fce2300586591baae6b0add2f86a74ad5b1af
Devki1 <gdev3195@gmail.com>
2020-02-28 09:18:08 +0530

    MODIFIED:Added comments and indentation

--- whileloop_repetition/MagicNumber.sh
+++ whileloop_repetition/MagicNumber.sh
@@ -1,20 +1,22 @@
 #!/bin/bash -x
 echo "guess the magic number"
+#Let take a value minimum and maximum
 minimum=1
 maximum=100
 number=-1
-	random_magic=$((RANDOM%100 +1))
-	while (($minimum<$maximum && $number!=$random_magic))
-	do
-		echo "guess number between $minimum and $maximum"
-		read number
-		if(($number == $random_magic))
-		then
-			echo "found $random_magic"
-		elif(($number <= $random_magic))
-		then
-			minimum=$number
-		else
-			maximum=$number
-		fi
-	done
+#Generate a random number
+random_magic=$((RANDOM%100 +1))
+while (($minimum<$maximum && $number!=$random_magic))
+do
+	echo "guess number between $minimum and $maximum"
+	read number
+	if(($number == $random_magic))
+	then
+		echo "found $random_magic"
+	elif(($number <= $random_magic))
+	then
+		minimum=$number
+	else
+		maximum=$number
+	fi
+done
